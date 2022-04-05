5fe300626658a086abcb4ed66b591ce6d8d6d7d8
Rudrank Riyam <rudrankriyam@gmail.com>
2022-04-05 19:58:20 +0530

    Initial commit

---
+++ HELLO WORLD/HELLO_WORLDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HELLO_WORLDApp.swift
+//  HELLO WORLD
+//
+//  Created by Rudrank Riyam on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HELLO_WORLDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
